7921433624026f92dd441b2992a722242fca661d
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-02-18 08:36:21 -0300

    Project created, EmailDTO

---
+++ src/main/java/com/devsuperior/integrations/resources/EmailResource.java
@@ -0,0 +1,15 @@
+package com.devsuperior.integrations.resources;
+
+import com.devsuperior.integrations.dto.EmailDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/emails")
+public class EmailResource {
+
+    @PostMapping
+    public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+        return ResponseEntity.noContent().build();
+    }
+}
