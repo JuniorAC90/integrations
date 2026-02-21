bd9b4995cb20530bf93bb6b438416397259b5a48
Aloizio Junior <66444799+JuniorAC90@users.noreply.github.com>
2026-02-21 13:36:32 -0300

    Strategy Pattern

--- src/main/java/com/devsuperior/integrations/resources/EmailResource.java
+++ src/main/java/com/devsuperior/integrations/resources/EmailResource.java
@@ -4,7 +4,10 @@ import com.devsuperior.integrations.dto.EmailDTO;
 import com.devsuperior.integrations.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/emails")
